9d5a8e2b3fd206b9a314c778696ce75a95648ebb
Sven Budak <sven@budak.info>
2018-12-21 11:07:01 +0100

    cleanup

--- projects/mark6-lib/src/lib/image/image.ts
+++ projects/mark6-lib/src/lib/image/image.ts
@@ -1,14 +1,22 @@
-import {Component, Directive, HostBinding, Input, ViewEncapsulation} from '@angular/core';
+import {Component, Directive, ElementRef, HostBinding, Input, OnInit, Renderer2, ViewChild, ViewEncapsulation} from '@angular/core';
 
 
 @Component({
     selector: 'mark6-image',
     exportAs: 'mark6Image',
-    templateUrl: './image.html',
+    // templateUrl: './image.html',
+    template: `
+        <ng-container #imgContainter *ngIf="imageLoaded">
+            <img (onload)="imageLoaded=true" class="mark6-image" *ngIf="src" [src]="src" [attr.alt]="alt"/>
+        </ng-container>
+        <div *ngIf="!imageLoaded" class="mark6-placeholder" [style.paddingTop.%]="100 / (width / height)" [style.backgroundColor]="backgroundColor"></div>
+    `,
     styleUrls: ['./image.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class Mark6ImageComponent {
+export class Mark6ImageComponent implements OnInit {
+
+    @ViewChild('imgContainer', {read: ElementRef}) container: ElementRef;
 
     @Input() public src = null;
     @Input() public alt = null;
@@ -17,6 +25,21 @@ export class Mark6ImageComponent {
     @Input() public backgroundColor = '#404040';
 
     @HostBinding('class') classes = 'mark6-image';
+
+    public imageLoaded = false;
+
+    constructor(private renderer: Renderer2) {}
+
+    ngOnInit() {
+        if (this.src) {
+            const img = this.renderer.createElement('img');
+            img.onload = () => {
+                this.imageLoaded = true;
+                this.renderer.appendChild(this.container.nativeElement, img);
+            };
+            img.src = this.src;
+        }
+    }
 
 }
 
