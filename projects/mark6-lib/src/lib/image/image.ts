72ca1aca5b9d364bf74809bca95e31ec6c350446
Murhaf Sousli <murhafsousli@gmail.com>
2019-01-01 09:19:44 +0300

    fixes resize issue and minimize options, closes #4

--- projects/mark6-lib/src/lib/image/image.ts
+++ projects/mark6-lib/src/lib/image/image.ts
@@ -1,7 +1,4 @@
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, TemplateRef } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { animate, style, transition, trigger } from '@angular/animations';
-import { BehaviorSubject } from 'rxjs';
+import { Component, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'mark6-image',
@@ -10,30 +7,17 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./image.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[class.mark6-image]': 'true',
-    '[class.mark6-image-load-success]': '!!imageUrl',
-    '[class.mark6-image-load-failed]': '!!loadError'
-  },
-  animations: [
-    trigger('fadeIn', [
-      transition(':enter', [
-        style({opacity: 0}),
-        animate('300ms ease-in', style({opacity: 1}))
-      ])
-    ])
-  ],
+    '[class.mark6-image]': 'true'
+  }
 })
 
 export class Mark6ImageComponent {
 
-  /** Stream that emits the state */
-  readonly state = new BehaviorSubject<'loading' | 'success' | 'failed'>('loading');
+  /** Image loading state */
+  state: 'loading' | 'success' | 'failed' = 'loading';
 
   /** Image URL after load success */
-  imageUrl: SafeUrl;
-
-  /** Load failed error */
-  loadError: Error;
+  url: string;
 
   /** Image source URL */
   @Input() src: string;
@@ -41,27 +25,18 @@ export class Mark6ImageComponent {
   /** Image alt attribute */
   @Input() alt: string;
 
-  /** Custom loader template */
-  @Input() loadingTemplate: TemplateRef<any>;
-
-  /** Custom error template */
-  @Input() errorTemplate: TemplateRef<any>;
-
-  /** Stream that emits when an error occurs */
-  @Output() error = new EventEmitter<Error>();
-
-  constructor(private _sanitizer: DomSanitizer) {
+  constructor(private _cd: ChangeDetectorRef) {
   }
 
-  loadSuccess(blobUrl: string) {
-    this.imageUrl = this._sanitizer.bypassSecurityTrustUrl(blobUrl);
-    this.state.next('success');
+  loadSuccess(url: string) {
+    this.url = url;
+    this.state = 'success';
+    this._cd.detectChanges();
   }
 
-  loadFailed(err: Error) {
-    this.loadError = err;
-    this.state.next('failed');
-    this.error.emit(err);
+  loadFailed() {
+    this.state = 'failed';
+    this._cd.detectChanges();
   }
 
 }
